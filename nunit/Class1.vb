4f1a3f8224ad05de5f19c51b4bbb325339110984
Hareendra Irugalratne <ihareendra@yahoo.com>
2016-01-03 20:20:32 +1000

    Files for nunit Testing on sorting application

---
+++ nunit/Class1.vb
@@ -0,0 +1,32 @@
+﻿Imports System.IO
+
+
+Public Class Form1
+
+   
+    Dim sortedNames As String
+
+   
+    Dim openFileDialog1 As String = "[SMITH" + "," + " ANDREW" + Environment.NewLine + "BAKER" + "," + "THEODORE]"
+   
+
+    Private Sub sortingNames()
+
+
+        'sorting the names from the rich textbox 
+        Dim LinesList As New List(Of String)
+        LinesList.AddRange(openFileDialog1.Split)
+
+        LinesList.Sort()
+
+
+
+        For Each Str As String In LinesList
+            sortedNames = (Str & Environment.NewLine)
+        Next
+
+        MsgBox(sortedNames)
+    End Sub
+
+   
+End Class
---
+++ nunit/nUnitTesting.vb
@@ -0,0 +1,36 @@
+﻿Imports NUnit.Framework
+Imports System
+
+
+<TestFixture()> Public Class nUnitTesting
+
+    Dim sortedNames As String
+    Dim openFileDialog1 As String
+
+    <SetUp()> Public Sub Init()
+       
+
+        openFileDialog1 = "[SMITH" + "," + " ANDREW" + Environment.NewLine + "BAKER" + "," + "THEODORE]"
+
+    End Sub
+
+    <Test()> Public Sub sortingNames()
+
+
+
+        'sorting the names from the rich textbox 
+        Dim LinesList As New List(Of String)
+        LinesList.AddRange(openFileDialog1.Split)
+
+        LinesList.Sort()
+
+
+
+        For Each Str As String In LinesList
+            sortedNames = (Str & Environment.NewLine)
+        Next
+
+        MsgBox(sortedNames)
+    End Sub
+
+End Class
